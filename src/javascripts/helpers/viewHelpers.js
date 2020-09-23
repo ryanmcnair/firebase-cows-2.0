f6f335cc39230a41f86e76013acaa59aa9f846f8
Ryan McNair <ryan.mcnair@gmail.com>
2020-09-23 07:09:35 -0500

    added farmer delete function, linked to firebase and delete cows function

--- src/javascripts/helpers/viewHelpers.js
+++ src/javascripts/helpers/viewHelpers.js
@@ -1,5 +1,6 @@
 import farmersView from '../components/views/farmersView';
 import cowsView from '../components/views/cowsView';
+import addCowsView from '../components/views/addCowView';
 
 const viewHelper = (id) => {
   $('#app').html('');
@@ -8,12 +9,15 @@ const viewHelper = (id) => {
       return farmersView.farmersView();
     case 'cows-link':
       return cowsView.cowsView();
+    case 'add-cow-link':
+      return addCowsView.addCowView();
     default:
       return console.warn('nothing clicked');
   }
 };
 
-const viewListener = () => {
+const viewListener = (view) => {
+  viewHelper(view);
   $('body').on('click', 'li.nav-item', (e) => {
     viewHelper(e.currentTarget.id);
   });
